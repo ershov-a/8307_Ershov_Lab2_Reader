8c01188bd9cf77f8a4998cbcafa286110cb1c0c4
ershov-a <5314011+ershov-a@users.noreply.github.com>
2020-10-30 23:40:00 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <windows.h>
+
+int main() {
+    char data[1024];
+
+    // Открываем проецируемый файл
+    HANDLE fileHandle = OpenFileMapping(FILE_MAP_ALL_ACCESS, FALSE, "mappedFile");
+    if (fileHandle != INVALID_HANDLE_VALUE) {
+        std::cout << "*** Mapping was opened successfully ***" << std::endl;
+    }
+
+
+    PVOID mapAddress = MapViewOfFile(fileHandle, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    if (mapAddress == nullptr){
+        std::cerr << "*** Can not open MapView of file ***" << std::endl;
+        return 1;
+    }
+
+    // Получаем данные из проекции
+    memcpy(data, (char*)mapAddress, 1024);
+
+    // Выводим адрес и данные
+    std::cout << "Value at "<< mapAddress <<" is " << data << std::endl;
+
+    char answer;
+    std::cout << "Close Mapping? (y/n) " << std::endl;
+    std::cin >> answer;
+    // "Разрушаем" связь спроецированного региона и его ресурсом
+    if (answer == 'y') {
+        UnmapViewOfFile(mapAddress);
+    }
+
+    // Закрываем HANDLE'ы
+    CloseHandle(fileHandle);
+
+    return 0;
+}
